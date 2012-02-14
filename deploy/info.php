86e112e5ebe20f30ccc8e3c4907b16475deda139
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-02-14 15:48:59 +0000

    - Cleaned up data

--- deploy/info.php
+++ deploy/info.php
@@ -3,15 +3,15 @@
 // General package information
 //----------------------------
 
-$package['name'] = 'theme-clearos6x';
-$package['title'] = 'ClearOS 6.x default theme';
-$package['description'] = 'ClearOS 6.x default theme';
+$package['name'] = 'theme-default';
+$package['title'] = 'Default theme';
+$package['description'] = 'Default theme';
 
 $package['version'] = '6.0';
 $package['release'] = '0.2';
 
 $package['vendor'] = 'ClearFoundation';
 $package['packager'] = 'ClearFoundation';
-$package['license'] = 'Copyright ClearFoundation 2009-2010.  All rights reserved.';
+$package['license'] = 'Copyright ClearFoundation 2010-2012.  All rights reserved.';
 
 // vim: ts=4 syntax=php
